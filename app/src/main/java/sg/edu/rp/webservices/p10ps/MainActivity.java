d0b5a16f58e74f651d0eee30db9bb0386de73453
18016375 <cecilialui16@yahoo.com.sg>
2020-07-23 19:21:12 +0800

    activities 1-100000000000000

--- app/src/main/java/sg/edu/rp/webservices/p10ps/MainActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/p10ps/MainActivity.java
@@ -1,9 +1,16 @@
 package sg.edu.rp.webservices.p10ps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +20,42 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    ArrayList<Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+
+    Button btnReadLater;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vPager = findViewById(R.id.viewpager1);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new FirstFragment());
+        al.add(new SecondFragment());
+        al.add(new ThirdFragment());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+
+        btnReadLater = findViewById(R.id.btnReadLater);
+
+        btnReadLater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int max = vPager.getChildCount();
+                if (vPager.getCurrentItem() < max-1){
+                    int nextPage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextPage, true);
+                }
+            }
+        });
+
     }
 }
