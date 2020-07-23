1789b905a41c7be99f1e69b4fe2138e29a53b33b
18016375 <cecilialui16@yahoo.com.sg>
2020-07-23 19:30:58 +0800

    activities 1-100000000000000

--- app/src/main/java/sg/edu/rp/webservices/p10ps/FirstFragment.java
+++ app/src/main/java/sg/edu/rp/webservices/p10ps/FirstFragment.java
@@ -1,5 +1,6 @@
 package sg.edu.rp.webservices.p10ps;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +60,25 @@ public class FirstFragment extends Fragment {
         }
     }
 
+    TextView tv1;
+    Button btnColour;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
+
+        btnColour = view.findViewById(R.id.btnColour);
+        btnColour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                view.setBackgroundColor(color);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/sg/edu/rp/webservices/p10ps/ThirdFragment.java
+++ app/src/main/java/sg/edu/rp/webservices/p10ps/ThirdFragment.java
@@ -1,5 +1,6 @@
 package sg.edu.rp.webservices.p10ps;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +60,23 @@ public class ThirdFragment extends Fragment {
         }
     }
 
+    TextView tv3;
+    Button btnColour;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_third, container, false);
+
+        btnColour = view.findViewById(R.id.btnColour);
+        btnColour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                view.setBackgroundColor(color);
+            }
+        });
     }
 }
